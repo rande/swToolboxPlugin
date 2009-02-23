5924d5643f20e4092c3164885a0d0f23b863b4b6
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-02-23 20:12:16 +0000

    improve swToolboxFormHelper
    git-svn-id: http://svn.symfony-project.com/plugins/swToolboxPlugin/sf1.2@15732 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- trunk/web/js/swToolbox.js
+++ trunk/web/js/swToolbox.js
@@ -21,7 +21,16 @@
  */
  
 var swToolbox = {
+  updateInProgress: false,
   updateFormElements: function(url, widget, class_name) {
+    if(swToolbox.updateInProgress)
+    {
+      alert('Update in progress, please wait !');
+      
+      return;
+    }
+    
+    swToolbox.updateInProgress = true;
     
     // get the parent form
     var form = widget.form;
@@ -42,10 +51,22 @@ var swToolbox = {
 
     for(var param in data) {      
       var elm = jQuery("#" + param, this.form);
-      if(data[param].value != undefined)
+      
+      if(data[param].html != undefined)
+      {
+        // we force the id, as some symfony widgets do not provide a main id, like radiobutton
+        var new_elm = jQuery(data[param].html);
+        new_elm.attr('id', param); 
+        
+        elm.replaceWith(new_elm);
+        elm = jQuery("#" + param, this.form);
+      }
+      else if(data[param].value != undefined)
       {
         elm.val(data[param].value);
       }
     }
+    
+    swToolbox.updateInProgress = false;
   }
 }
