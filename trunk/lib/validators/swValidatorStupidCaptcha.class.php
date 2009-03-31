c1d4a8a2678afa387502f081b85dd9b76973461b
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-03-31 13:59:50 +0000

    add stupid captcha validator
    git-svn-id: http://svn.symfony-project.com/plugins/swToolboxPlugin/sf1.2@16808 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ trunk/lib/validators/swValidatorStupidCaptcha.class.php
@@ -0,0 +1,51 @@
+<?php
+
+
+/*
+ * (c) 2008 Thomas Rabaix <thomas.rabaix@soleoweb.com>
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.soleoweb.com>.
+ */
+
+/**
+ * 
+ * Make sure the input is only text : a-Z, 0-1, "\n"
+ * 
+ * @author Thomas Rabaix <thomas.rabaix@gmail.com>
+ * @license     http://www.opensource.org/licenses/lgpl-license.php LGPL
+ * @link        www.soleoweb.com
+ * 
+ * SVN : $Id$
+ **/
+class swValidatorStupidCaptcha extends sfValidatorBase
+{
+
+   public function doClean($value)
+   {
+     if(strlen($value) > 0)
+     {
+       throw new sfValidatorError($this, 'captcha', array('value' => $value,));
+     }
+     
+     return $value;
+   }
+   
+   public function isEmpty($value)
+   {
+     return !parent::isEmpty($value);
+   }
+}
