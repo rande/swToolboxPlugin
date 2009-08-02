4b250c74283bd245e8cfa051a3eb5b93cb4bd0ad
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-08-02 20:30:15 +0000

    add license imformation
    git-svn-id: http://svn.symfony-project.com/plugins/swToolboxPlugin/sf1.2/trunk@20708 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/menu/swMenuManager.class.php
+++ lib/menu/swMenuManager.class.php
@@ -1,5 +1,32 @@
 <?php
+/*
+ *  $Id$
+ *
+ * (c) 2008 Thomas Rabaix <thomas.rabaix@soleoweb.com>
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the LGPL. For more information, see
+ * <http://www.soleoweb.com>.
+ */
 
+/**
+ *
+ * @package    swToolboxPlugin
+ * @author     Thomas Rabaix <thomas.rabaix@soleoweb.com>
+ * @version    SVN: $Id$
+ */
 class swMenuManager
 {
   
@@ -9,7 +36,9 @@ class swMenuManager
   protected
     $menu_handlers = array(),
     $menus = array(),
-    $bound = false;
+    $bound = false,
+    $selector = null,
+    $references = array();
   
   private function __construct()
   {
@@ -28,6 +57,17 @@ class swMenuManager
     }
     
     return self::$instance;
+  }
+  
+  public function setSelector($selector)
+  {
+    $this->selector = $selector;
+  }
+  
+  public function getSelector()
+  {
+    
+    return $this->selector;
   }
   
   public function getMenu($name)
