31543c6aae476789f446075f8815729b485c0603
Thomas Rabaix <thomas.rabaix@soleoweb.com>
2009-08-02 20:28:57 +0000

    improve swMail classes
    git-svn-id: http://svn.symfony-project.com/plugins/swToolboxPlugin/sf1.2/trunk@20707 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/mail/swMail.class.php
+++ lib/mail/swMail.class.php
@@ -28,48 +28,99 @@
  * @author     Thomas Rabaix <thomas.rabaix@soleoweb.com>
  * @version    SVN: $Id$
  */
-class swMail extends Zend_Mail
-{
-  public function __construct($charset = null)
-  {
-    if(is_null($charset))
-    {
+class swMail extends Zend_Mail {
+  public function __construct($charset = null) {
+    if(is_null($charset)) {
       $config = sfConfig::get('app_swToolbox_mail');
       $charset = $config['charset'];
     }
-    
+
     parent::__construct($charset);
   }
-  
-  public function quickView()
-  {
+
+  public function quickView() {
     $mail = "";
-    foreach($this->getParts() as $part)
-    {
+    foreach($this->getParts() as $part) {
       $mail .= $part->getContent();
     }
-    
+
     return $mail;
   }
-  
-  public function __toString()
-  {
-    
+
+  public function __toString() {
+
     return $this->quickView();
   }
-  
-  public function getRecipientsTo()
-  {
+
+  public function getRecipientsTo() {
+    
     return $this->_to;
   }
-  
-  public function getRecipientsBcc()
-  {
+
+  public function getRecipientsBcc() {
+
     return $this->_to;
   }
-  
-  public function getRecipientsCc()
-  {
+
+  public function getRecipientsCc() {
+
     return $this->_to;
+  }
+
+  public function getHeader($name) {
+    if(isset($this->_headers[$name])) {
+      
+      return $this->_headers[$name];
+    }
+
+    return null;
+  }
+
+  public function getPrintableTo()
+  {
+    
+    return $this->_getPrintable($this->getHeader('To'));
+  }
+
+  private function _getPrintable($tos)
+  {
+    if(!is_array($tos))
+    {
+
+      return '';
+    }
+
+    $to = array();
+    foreach($tos as $name => $email)
+    {
+
+      if($name === 'append')
+      {
+
+        continue;
+      }
+
+      $to[] = $email;
+    }
+
+    return implode(", ", $to);
+  }
+  
+  public function getPrintableBcc()
+  {
+
+    return $this->_getPrintable($this->getHeader('Bcc'));
+  }
+
+  public function getPrintableCc()
+  {
+
+    return $this->_getPrintable($this->getHeader('Cc'));
+  }
+
+  public function getPrintableReplyTo()
+  {
+
+    return $this->_getPrintable($this->getHeader('ReplyTo'));
   }
 }
